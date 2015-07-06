3c6c8ca03d8fb375145b7739dc84c2666d9b2049
binlyzhuo <binlyzhuo@outlook.com>
2015-07-06 22:36:16 +0800

    ~commit

--- ProfCharp5/ProCsharp/Program.cs
+++ ProfCharp5/ProCsharp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
 
 namespace ProCsharp
 {
@@ -11,6 +12,12 @@ namespace ProCsharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("File Project!!");
+            FileInfo file = new FileInfo(@"E:\bitbucket\authframework\src\AuthFramework.Web\Controllers\DeptController.cs");
+            bool isExist = file.Exists;
+            DirectoryInfo dirinfo = new DirectoryInfo(@"E:\bitbucket\authframework\src\AuthFramework.Web\Controllers");
+            Console.WriteLine(Path.Combine(@"e:\file","cc.cs"));
+            Console.ReadLine();
         }
     }
 }
